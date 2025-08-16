49df9dba23e1ded2c7a8cea444c0afcb1945d37b
claude3d <35361720+claude3d@users.noreply.github.com>
2025-08-15 20:51:46 -0400

    Import of cesinvn

---
+++ cesinvn/Messages_files/Messages.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20200119
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Messages_files/MessagesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
