c874b572c207b3abde97f9d8690b3f8cac7678d3
claude3d <35361720+claude3d@users.noreply.github.com>
2025-08-18 19:34:33 -0400

    remove claude3d folder

--- claude3d/Home_files/Home.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20180728
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Home_files/HomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
--- claude3d/Virtual_telescope_files/Virtual_telescope.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20190715
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Virtual_telescope_files/Virtual_telescopeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
